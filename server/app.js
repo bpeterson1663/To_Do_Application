be2389ca8a136ff07661a2bbcb7cc8b2da78bfd8
bpeterson1663 <bpeterson1663@gmail.com>
2016-07-06 12:03:49 -0500

    server update

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = express();
-var index = require('./routes/index');
+var path = require('path');
+var index = require('./routes/index.js');
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", index);
 
 app.set("port", process.env.PORT || 3000);
 
-app.listen((app.get("port")), function(){
-  console.log("Listening on Port :3000");
+app.listen((app.get('port')),function(){
+  console.log("Listening on port: 3000");
 });
